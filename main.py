4b2897d839d2f6cef5bd62f0b946d52b0b798c2e
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2022-10-11 14:12:09 +1100

    munging

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
-# begin with jump test
-from curses import KEY_UP
-import sys, pygame
-pygame.init()
 
-size = width, height = 640, 480
-speed = [1, 1]
-black = 0, 0, 0
+import pygame # import
+pygame.init() # init
 
-screen = pygame.display.set_mode(size)
+win = pygame.display.set_mode((500,500)) # make display
+pygame.display.set_caption("First Game") # set title
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+# init vars for player
+x = 250 # starting screen pos for player
+y = 400 # ""
+width = 20 # width of player
+height = 30 # height of player
+vel = 5 # steps per tick of move
 
-while 1: #game loop
-    for event in pygame.event.get(): # if quit window then stop
-        if event.type == pygame.QUIT: sys.exit()
+# init vars for testing platform
+platX = 350
+platY = 300
+platWidth = 125
+platHeight = 20
+
+# list of platform objects
+platforms = [pygame.Rect(platX,platY,platWidth,platHeight)]
+
+isJump = False # keeps track of jumping
+jumpCount = 10 # has to do with jump height
+
+run = True
+
+while run:
+    pygame.time.delay(25) # time per frame
+
+    for event in pygame.event.get(): # exiting program
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed() # get keypresses
     
-    if (pygame.key.getpressed()[pygame.K_UP]):
-        print('up key pressed')
+    if keys[pygame.K_LEFT] and x > vel: # make sure no go out of bounding box x left - will not need later on
+        x -= vel
 
-    # start drawing the next screen
-    screen.fill(black) # make the screen blank
-    screen.blit(ball, ballrect) # draw on the rectangle in its new location
-    pygame.display.flip() # show new screen
+    if keys[pygame.K_RIGHT] and x < 500 - vel - width:  # make sure no go out of bounding box right - will not need later on
+        x += vel
+        
+    if not(isJump): # can jump if not already jumping
+        if keys[pygame.K_UP]:
+            isJump = True
+    else: # jump logic
+        
+        if jumpCount >= -10:
+            y -= (jumpCount * abs(jumpCount)) * 0.5
+            jumpCount -= 1
+        else: 
+            jumpCount = 10
+            isJump = False
+    
+    # draw next frame
+    win.fill((0,0,0)) # bg
+    pygame.draw.rect(win, (255,0,0), (x, y, width, height)) # player
+
+    for plat in platforms: # draw all platforms
+        pygame.draw.rect(win, (0,125,255), plat)
+
+    pygame.display.update() 
+    
+pygame.quit()
