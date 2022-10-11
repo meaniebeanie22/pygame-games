2fcce73fae8cea888c21467e849f96824a4d7724
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2022-10-12 08:52:03 +1100

    add more stuff

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Player(pygame.sprite.Sprite): # player is a sprite
         self.acc = vec(0,0) # acceleration as a vector
  
     def move(self):
-        self.acc = vec(0,0) # starting point for acceleration
+        self.acc = vec(0,0.5) # starting point for acceleration - no x and down 0.5 for y (for gravity)
     
         pressed_keys = pygame.key.get_pressed() # get pressed keys           
         if pressed_keys[K_LEFT]: # if left is pressed
@@ -45,6 +45,18 @@ class Player(pygame.sprite.Sprite): # player is a sprite
             self.pos.x = WIDTH # tp to otherside
             
         self.rect.midbottom = self.pos  # reference point for char = middle bottom of hitbox (for collisions)  
+    
+    def update(self): #update to make sure we not sad
+        hits = pygame.sprite.spritecollide(P1 ,platforms, False) # get collisions
+        if P1.vel.y > 0: # if we're moving
+            if hits: # and there's any hits
+                self.vel.y = 0 # then stop us
+                self.pos.y = hits[0].rect.top + 1 # and move us to the top of the platform
+     
+    def jump(self): # jump 
+        hits = pygame.sprite.spritecollide(self, platforms, False) # return platforms we're touching
+        if hits: # if there are any, then we can jump
+           self.vel.y = -15 # jump with 15u of beans
  
 class platform(pygame.sprite.Sprite): # platforms are sprites
     def __init__(self): # make a platform
@@ -52,10 +64,16 @@ class platform(pygame.sprite.Sprite): # platforms are sprites
         self.surf = pygame.Surface((WIDTH, 20)) # a platform is a surface
         self.surf.fill((255,0,0)) # fill it
         self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10)) # move it to a pos for testing
- 
+    
+    def move(self): # placeholder to deal with scrolling
+        pass
+
 PT1 = platform() # pt1 is a plat
 P1 = Player() # player is a player
  
+platforms = pygame.sprite.Group() # make a group called platforms
+platforms.add(PT1) # add test platform to it
+
 all_sprites = pygame.sprite.Group() # make a group for our stuff
 all_sprites.add(PT1) # add plat to it
 all_sprites.add(P1) # add player to it
@@ -65,12 +83,17 @@ while True: # game loop
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-     
+        if event.type == pygame.KEYDOWN:    # if the player is pressing a key
+            if event.key == pygame.K_SPACE: # is it space?
+                P1.jump() # call the jump method
+    
+
     displaysurface.fill((0,0,0)) # make the screen
- 
-    P1.move() # move the player for this frame
-    for entity in all_sprites: # for all thigns
+    P1.update() # update the player for this frame
+
+    for entity in all_sprites: # for all things
         displaysurface.blit(entity.surf, entity.rect) # draw them
+        entity.move() # move the entity (if it can atm)
     
     pygame.display.update() # update the display
     FramePerSec.tick(FPS) # tick at ratelimit
